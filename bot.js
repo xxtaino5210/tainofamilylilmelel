e6d6368d14c2df73a92e905bb331711cd93d86af
xxtaino5210 <42452904+xxtaino5210@users.noreply.github.com>
2018-08-17 12:26:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,4 +64,30 @@ if (command == "tweet") {
 
 
 });
+
+client.on('message', message =>{
+    let args = message.content.split(' ');
+    let prefix = '$'; 
+    
+    if(args[0] === `${prefix}avatar`){
+        let mentions = message.mentions.members.first()
+        if(!mentions) {
+          let sicon = message.author.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setImage(message.author.avatarURL)
+          .setColor("#f7abab") 
+          .setDescription(`**${message.author.username}#${message.author.discriminator}**'s avatar :`);
+          message.channel.send({embed})
+        } else {
+          let sicon = mentions.user.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setColor("#f7abab")
+          .setDescription(`**${mentions.user.username}#${mentions.user.discriminator}**'s avatar :`)
+          .setImage(sicon)
+          message.channel.send({embed})
+        }
+    };
+});
+
+
 client.login(process.env.BOT_TOKEN);
