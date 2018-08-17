fd1ba22f28bfe820d8c2a965cbd76c68e436dcd4
xxtaino5210 <42452904+xxtaino5210@users.noreply.github.com>
2018-08-17 16:59:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,6 @@ const request = require('request');
 const fs = require('fs');
 const getYouTubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
-
-var config = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
-
-const yt_api_key = config.yt_api_key;
-const bot_controller = config.bot_controller;
-const discord_token = config.discord_token;
 const prefix = '$'
 
 client.on('ready', () => {
