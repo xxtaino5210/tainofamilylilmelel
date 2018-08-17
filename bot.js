f21e02b4fb94a464c38534c812c34e88f4fe5010
xxtaino5210 <42452904+xxtaino5210@users.noreply.github.com>
2018-08-17 17:19:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
-
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const prefix = '$'
+const client = new Discord.Client();
+const prefix = '.'
 
-
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`working on`,"http://twitch.tv/S-F")
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -28,6 +27,5 @@ client.user.setGame(`working on`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-
 
 client.login(process.env.BOT_TOKEN);
