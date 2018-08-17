8c98a7ecc40aafdfac18111079e34e70d02ab8f9
xxtaino5210 <42452904+xxtaino5210@users.noreply.github.com>
2018-08-17 16:56:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,18 @@
+
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const db = require('quick.db');
-const giphy = require('giphy-api')();    
-const googl = require('goo.gl');  
-const translate = require('google-translate-api');   
-const fs = require("fs"); 
-const getYoutubeID = require('get-youtube-id'); 
-const moment = require("moment");  
-const { Client, Util } = require('discord.js');  
-const UserBlocked = new Set(); 
-const jimp = require('jimp');   
-const math = require('math-expression-evaluator'); 
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const google = require('google-it'); 
-const queue = new Map(); 
-const zalgo = require('zalgolize');   
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
+const bot = new Discord.Client();
 const ytdl = require('ytdl-core');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const sql = require("sqlite");
-const dateFormat = require('dateformat'); 
-const pretty = require('pretty-ms') 
+const request = require('request');
+const fs = require('fs');
+const getYouTubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+
+var config = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
+
+const yt_api_key = config.yt_api_key;
+const bot_controller = config.bot_controller;
+const prefix = config.prefix;
+const discord_token = config.discord_token;
 const prefix = '$'
 
 client.on('ready', () => {
