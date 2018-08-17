c32d94e251e5b59a455aacc455466b612d71e16d
xxtaino5210 <42452904+xxtaino5210@users.noreply.github.com>
2018-08-17 16:12:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const db = require('quick.db');
-const client = new Discord.Client();   
 const giphy = require('giphy-api')();    
 const googl = require('goo.gl');  
 const translate = require('google-translate-api');   
